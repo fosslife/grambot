6ea21b4c2bdf3046c4aab2efe38a976babb1e2ea
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-09 14:37:03 +0530

    added quote()

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient, events
 import os
+import requests
 
 api_id = os.environ['apiid']
 api_hash = os.environ['apihash']
@@ -15,11 +16,20 @@ async def messagelistener(event):
         # Can use .reply('Sup') if want to reply as `reply`
         await event.respond('Sup')
 
-# reply only if the incoming message is from @abcd
-@client.on(events.NewMessage(chats=('abcd')))
+# reply only if the incoming message is from @Sparkenstein
+@client.on(events.NewMessage(chats=('Sparkenstein')))
 async def fromsecondaccound(event):
     if 'fp' in event.raw_text:
-        await event.respond('Functional Programming')    
+        await event.respond('Functional Programming')
+
+# Similarly, you can use incoming=True for messages that you receive
+@client.on(events.NewMessage(pattern='quote()'))
+async def sendQute(event):
+    quotes_api_url = "https://opinionated-quotes-api.gigalixirapp.com/v1/quotes"
+    data = requests.get(quotes_api_url).json()
+    quote = data['quotes'][0]['quote']
+    author = data['quotes'][0]['author']
+    await event.reply(f'`{quote}`\n                       - __{author}__')
 
 # client.send_message('Sparkenstein', 'Hello World from sparks userbot')
 
