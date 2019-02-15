7a9166e2120d7834fc4e6a661e8a33006089bc52
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-15 07:15:33 +0530

    Added insults

---
+++ plugins/insults.py
@@ -0,0 +1,16 @@
+from random import choice
+from userbot import bot
+from telethon import TelegramClient, events
+from os import getcwd
+
+@bot.on(events.NewMessage(pattern='.die()'))
+async def insult(event):
+    pattern_string = event.pattern_match.string
+    entity = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
+    print(entity)
+    if entity:
+        await event.respond(entity)
+    else:
+        file = open("./plugins/data/insults").read().split("\n")
+        insult = choice(file)
+        await event.respond(insult)
