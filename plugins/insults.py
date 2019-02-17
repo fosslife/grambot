c59833da5c7b846f7deb83b7b21670bc9b737056
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-17 12:06:13 +0530

    reply to insult working

--- plugins/insults.py
+++ plugins/insults.py
@@ -2,18 +2,23 @@ from random import choice
 from userbot import bot
 from telethon import TelegramClient, events
 from os import getcwd
-from telethon.client.messages import MessageMethods
 
 @bot.on(events.NewMessage(pattern='.die()'))
 async def insult(event):
+    await event.delete()
     pattern_string = event.pattern_match.string
     entity = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
     file = open("./plugins/data/insults").read().split("\n")
     insult = choice(file)
-    # print(dir(event.message.to_id))
-    reply_to_user = event.message.to_id.channel_id or event.message.to_id.user_id
+    # print(event.message.to_id.stringify())
+    try:
+        reply_to_user = event.message.to_id.user_id
+    except AttributeError:
+        reply_to_user = event.message.to_id.channel_id
     reply_to_message = event.message.reply_to_msg_id
-    print("Replying to ", reply_to_user, reply_to_message)
+    if reply_to_message:
+        await bot.send_message(reply_to_user, message=insult, reply_to=reply_to_message)
+    else:
+        await bot.send_message(reply_to_user, message=insult)
 
-    await event.delete()
-    await event.respond(insult)
+    # await event.respond(insult)
