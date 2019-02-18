8091ed1de34f51be45a7a0339823c2de1223c8e8
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-18 22:07:11 +0530

    fixed animation for replies from other users

--- plugins/anim.py
+++ plugins/anim.py
@@ -9,8 +9,13 @@ async def anim(event):
     lst = animes.split(",")
     before = lst[0].strip()
     after = lst[1].strip()
-    for i in range(0, 2):
-        await event.edit(before)
+    try:
+        reply_to_user = event.message.to_id.user_id
+    except AttributeError:
+        reply_to_user = event.message.to_id.channel_id
+    sent = await bot.send_message(reply_to_user, message=after)
+    for i in range(0, 6):
+        await bot.edit_message(reply_to_user, sent.id, before)
         sleep(0.5)
-        await event.edit(after)
+        await bot.edit_message(reply_to_user, sent.id, after)
         sleep(0.5)
