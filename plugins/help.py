1aa353e937e33c1ad205725ca6cb44298e401227
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-18 18:32:50 +0530

    moved help to file

--- plugins/help.py
+++ plugins/help.py
@@ -4,8 +4,5 @@ from plugins import All_PLUGINS
 
 @bot.on(events.NewMessage(pattern='.help()'))
 async def help(event):
-    print(All_PLUGINS)
-    loaded_modules = ""
-    for modulename in All_PLUGINS:
-        loaded_modules += "." + modulename + "()\n"
-    await event.respond(loaded_modules)
+    commands = open("./plugins/data/commands").read()
+    await event.respond(commands)
