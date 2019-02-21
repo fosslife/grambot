6e84468a91c03ab6381058a95020ba2347312fd3
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-21 22:48:58 +0530

    improved repond types for reply

--- plugins/die.py
+++ plugins/die.py
@@ -8,15 +8,4 @@ async def insult(event):
     entity = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
     file = open("./plugins/data/insults").read().split("\n")
     insult = choice(file)
-    # print(event.message.to_id.stringify())
-    try:
-        reply_to_user = event.message.to_id.user_id
-    except AttributeError:
-        reply_to_user = event.message.to_id.channel_id
-    reply_to_message = event.message.reply_to_msg_id
-    if reply_to_message:
-        await bot.send_message(reply_to_user, message=insult, reply_to=reply_to_message)
-    else:
-        await bot.send_message(reply_to_user, message=insult)
-
-    # await event.respond(insult)
+    await event.respond(insult, reply_to=event.reply_to_msg_id)
