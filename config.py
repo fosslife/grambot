29669dd9c422a097eb165282eed6b6b042cad97e
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-23 13:39:13 +0530

    added test group in whitelist

--- config.py
+++ config.py
@@ -2,40 +2,40 @@ weather = {
     "pattern": "\.weather",
     "incoming": True,
     "outgoing": True,
-    "chats": (-1001335202320)
+    "chats": (-1001335202320, 1377536264)
 }
 
 user = {
     "pattern": "\.user",
     "incoming": True,
     "outgoing": True,
-    "chats": (-1001335202320)
+    "chats": (-1001335202320, 1377536264)
 }
 
 quote = {
     "pattern": "\.quote",
     "incoming": True,
     "outgoing": True,
-    "chats": (-1001335202320)
+    "chats": (-1001335202320, 1377536264)
 }
 
 help = {
     "pattern": "\.help",
     "incoming": True,
     "outgoing": True,
-    # "chats": (-1001335202320)
+    "chats": (-1001335202320, 1377536264)
 }
 
 die = {
     "pattern": "\.die",
     "incoming": True,
     "outgoing": True,
-    "chats": (-1001335202320)
+    "chats": (-1001335202320, 1377536264)
 }
 
 anim = {
     "pattern": "\.anim",
     "incoming": False,
     "outgoing": True,
-    "chats": (-1001335202320)
+    "chats": (-1001335202320, 1377536264)
 }
