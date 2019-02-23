f285f1ce94bcf245903b1ac572d4b64726152289
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-23 16:12:50 +0530

    delete plugin unstable

---
+++ plugins/deletes.py
@@ -0,0 +1,11 @@
+from userbot import bot
+from telethon import TelegramClient, events
+
+
+@bot.on(events.MessageDeleted())
+async def delted(event):
+    try:
+        id = event.original_update.channel_id
+    except Exception:
+        id = None
+    await bot.send_message('me', 'someone deleted something')
