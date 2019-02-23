c9f8271924f5d2c2da10bddd1c20d778c1b2f184
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-24 01:47:37 +0530

    issue with tagging fixed

--- plugins/tag.py
+++ plugins/tag.py
@@ -8,5 +8,5 @@ async def tag(event):
     pattern_string = event.pattern_match.string
     params = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
     as_user, orig_user = params.split(",")
-    msg = f"<a href='tg://{orig_user.strip()}/'>{as_user.strip()}</a>"
+    msg = f"<a href='http://t.me/{orig_user.strip()}/'>{as_user.strip()}</a>"
     await event.respond(msg, reply_to=event.reply_to_msg_id, parse_mode="HTML")
