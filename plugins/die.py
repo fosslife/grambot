8c7333100a6bcc62ec9995988d980c191f55ece5
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-23 15:44:24 +0530

    removed unused code

--- plugins/die.py
+++ plugins/die.py
@@ -5,8 +5,6 @@ from config import die
 
 @bot.on(events.NewMessage(**die))
 async def insult(event):
-    pattern_string = event.pattern_match.string
-    entity = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
     file = open("./plugins/data/insults").readlines()
     insult = choice(file)
     await event.respond(insult, reply_to=event.reply_to_msg_id)
