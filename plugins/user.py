1409e93e7ca74632a3df0aed32099e5db9e7fe4d
Sparkenstein <zetabytes.pp@gmail.com>
2019-02-23 15:45:52 +0530

    masked username links in exception

--- plugins/user.py
+++ plugins/user.py
@@ -1,17 +1,24 @@
 from userbot import bot
 from telethon import TelegramClient, events
 from config import user
+from telethon.tl.functions.users import GetFullUserRequest
 
 @bot.on(events.NewMessage(**user))
 async def getUser(event):
     pattern_string = event.pattern_match.string
     entity = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
     try:
-        info = await bot.get_entity(entity)
-        await event.respond(f"""Username - `{info.username}`
-{"User is restricted for " + info.restriction_reason  if info.restricted else "User is not restricted"}
-Name - {info.first_name} {info.last_name if info.last_name else ""}
-id - {info.id}
+        info = await bot(GetFullUserRequest(entity))
+        await event.respond(f"""
+Username - `{info.user.username}`
+{"User is a bot" if info.user.bot else "user is not a bot"}
+{"User is restricted for " + info.user.restriction_reason  if info.user.restricted else "User is not restricted"}
+Name - {info.user.first_name} {info.user.last_name if info.user.last_name else ""}
+Status - {info.about}
+id - {info.user.id}
+{info.common_chats_count} groups common with me
+{"I have blocked them" if info.blocked else "I have not blocked them"}
+
 """)
     except Exception:
-        await event.respond("Cannot find entity with " + entity)
+        await event.respond(f"Cannot find entity with `{entity}`")
