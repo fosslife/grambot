2fc9e95291513e5d0f492e2e160ec4a2731d95a9
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-10 22:34:12 +0530

    shortened logic for myname plugin

--- plugins/myname.py
+++ plugins/myname.py
@@ -11,13 +11,4 @@ async def myname(event):
         present = [x for x in users if x.lower() in event.raw_text.lower()]
         if present:
             group_id = event.message.to_id.channel_id
-            group_entity = await bot.get_entity(group_id)
-            group_title = group_entity.username
-            link = "["+group_entity.title+"](http://t.me/"+group_title + ")" if group_title else group_entity.title
-            print(link)
-            # get user
-            from_id = event.message.from_id
-            user_entity = await bot.get_entity(from_id)
-            current_time = datetime.now().strftime("%d-%B-%Y at %H:%M")
-            message = f"{user_entity.username} from {link} group said something about you on {current_time}\nThe text was:\n{event.raw_text}"
-            await bot.send_message('me', message=message, link_preview=False)
+            await bot.forward_messages('me', event.message.id, group_id)
