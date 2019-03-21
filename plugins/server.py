fb22649c18d7738db08b041c9ba83a72da760c74
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 11:27:30 +0530

    added server plugin logger

--- plugins/server.py
+++ plugins/server.py
@@ -1,4 +1,4 @@
-from userbot import bot
+from userbot import bot, logger
 from telethon import TelegramClient, events
 from config import server
 import subprocess
@@ -10,12 +10,16 @@ allowed_commands = [
     'cd',
     'mkdir',
 ]
+
 @bot.on(events.NewMessage(**server))
 async def server(event):
+    logger.info("server plugin called")    
     pattern_string = event.pattern_match.string
     command = pattern_string[pattern_string.find("(")+1:pattern_string.find(")")]
+    logger.info(f"command to execute - {command}")
     parametrize = command.split(" ")
     pipe = subprocess.check_output(parametrize, stderr=subprocess.PIPE)
     message = pipe.decode('utf-8')
     if command in allowed_commands:
+        logger.info("command is in allowed_commands")
         await event.respond(f"```{message}```")
