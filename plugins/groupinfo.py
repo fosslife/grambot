8e62bc1511c0ad29317b862cf013579ca99bd280
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 11:17:13 +0530

    added logs for ids

--- plugins/groupinfo.py
+++ plugins/groupinfo.py
@@ -1,13 +1,16 @@
-from userbot import bot
+from userbot import bot, logger
 from telethon import TelegramClient, events
 from config import id
 
 
 @bot.on(events.NewMessage(**id))
 async def id(event):
+    logger.info("group info plugin called")
     try:
         id = event.message.to_id.channel_id
+        logger.info(f"sending group id - {id}")
         await event.respond(f"groupid - {id}")
     except AttributeError:
         id = event.message.to_id.user_id
+        logger.info("sending user id - {id}")
         await event.respond(f"userid - {id}")
