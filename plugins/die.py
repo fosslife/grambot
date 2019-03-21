973aee7382c63c331d44fa8767ef192e73cdd748
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 11:16:49 +0530

    added logs for insults

--- plugins/die.py
+++ plugins/die.py
@@ -1,10 +1,12 @@
 from random import choice
-from userbot import bot
+from userbot import bot, logger
 from telethon import TelegramClient, events
 from config import die
 
 @bot.on(events.NewMessage(**die))
 async def insult(event):
+    logger.info("insults plugin called")
     file = open("./plugins/data/insults").readlines()
     insult = choice(file)
+    logger.info(f"chosen insult - {insult}")
     await event.respond(insult, reply_to=event.reply_to_msg_id)
