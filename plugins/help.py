f1e4ff5bfa1df56579cafcb01da8d2276e9c3bf3
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 11:17:57 +0530

    added help plugin loggers

--- plugins/help.py
+++ plugins/help.py
@@ -1,8 +1,9 @@
-from userbot import bot
+from userbot import bot, logger
 from telethon import TelegramClient, events
 from config import help
 
 @bot.on(events.NewMessage(**help))
 async def help(event):
+    logger.info("help plugin called")
     commands = open("./plugins/data/commands").read()
     await event.respond(commands)
