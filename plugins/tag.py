ab6a4a46582760ec4fddc7e8828006bfd1adc7f8
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 11:27:18 +0530

    added quotes and tags logger

--- plugins/tag.py
+++ plugins/tag.py
@@ -1,8 +1,9 @@
-from userbot import bot
+from userbot import bot, logger
 from telethon import TelegramClient, events
 from config import tag
 
 @bot.on(events.NewMessage(**tag))
 async def tag(event):
+    logger.info("tag plugin is called")
     await event.delete()
     await event.respond(event.raw_text, reply_to=event.reply_to_msg_id)
