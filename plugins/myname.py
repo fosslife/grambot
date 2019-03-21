d123a175141e76f29ec796455831e113468139e2
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 11:24:48 +0530

    added myname plugin logs

--- plugins/myname.py
+++ plugins/myname.py
@@ -1,4 +1,4 @@
-from userbot import bot
+from userbot import bot, logger
 from telethon import TelegramClient, events
 from config import myname
 from os import environ
@@ -6,9 +6,13 @@ from datetime import datetime
 
 @bot.on(events.NewMessage(**myname))
 async def myname(event):
+    logger.info("myname plugin is called")
     if event.is_group:
         users = environ.get("my_name_aliases", "").split(' ')
+        logger.info(f"my name aliases are {users}")
+        logger.info(f"incoming message is {event.raw_text.lower()}")
         present = [x for x in users if x.lower() in event.raw_text.lower()]
         if present:
+            logger.info(f"your name present in incoming message, forwarding it to saved message")
             group_id = event.message.to_id.channel_id
             await bot.forward_messages('me', event.message.id, group_id)
