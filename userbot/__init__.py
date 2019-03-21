f7be0108a56ad6c791f4bdeedce05d21be8cc21c
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-21 10:27:21 +0530

    added rotating log files

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,24 +1,30 @@
 from telethon import TelegramClient, events
 import os
 
-
 import logging
+import logging.handlers
 from telethon import TelegramClient, events
 import time
 import os
 from dotenv import load_dotenv
 
-logging.basicConfig(
-    filename="logs/grambot.log",
-    filemode='a',
-    datefmt='%H:%M:%S',
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-
-load_dotenv()
+LOG_FILENAME = 'logs/grambot.log'
+logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+needRoll = os.path.isfile(LOG_FILENAME)
+
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, backupCount=20)
+handler.setFormatter()
+logger.addHandler(handler)
+load_dotenv()
+
+
+if needRoll:    
+    logger.handlers[0].doRollover()
+
 
 api_id = os.environ['apiid']
 api_hash = os.environ['apihash']
