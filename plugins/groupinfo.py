7ef18492a6a1749f8af67811444f80e24b2e3b31
Sparkenstein <zetabytes.pp@gmail.com>
2020-10-25 17:19:38 +0530

    something idk

--- plugins/groupinfo.py
+++ plugins/groupinfo.py
@@ -12,5 +12,9 @@ async def fn(event):
         await event.respond(f"groupid - {id}")
     except AttributeError:
         id = event.message.to_id.user_id
-        logger.info("sending user id - {id}")
+        logger.info(f"sending user id - {id}")
         await event.respond(f"userid - {id}")
+    except Exception as e:
+        logger.exception(f"Error while fetching records {e}")
+        return
+
