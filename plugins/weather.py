a706e2f48a42b0732dc8af7188f41795a8c0007a
Sparkenstein <zetabytes.pp@gmail.com>
2023-05-19 23:02:23 +0530

    feat: add air pollution

--- plugins/weather.py
+++ plugins/weather.py
@@ -11,33 +11,22 @@ async def get_weather(event):
     logger.info("weather plugin called")
     query = event.pattern_match.string.split(" ")
     logger.info(f"weather query - {query}")
-    try:
-        city_to_find = query[1]
-    except IndexError:
-        logger.info("No city provided")
+    openweather_api_key = environ.get("openweather_api_key", None)
+
+    if len(query) == 1:
+        logger.info(f"no city provided")
         await event.respond("Please provide a city name")
         return
-    try:
-        is_forecast = query[2] or None
-    except IndexError:
-        is_forecast = None
-    # print(is_forecast)
-    logger.info(f"city to find - {city_to_find}")
+    elif len(query) == 2:
+        city_to_find = query[1]
+        logger.info(f"city to find - {city_to_find}")
 
-    # Prepare request
-    openweather_api_key = environ.get("openweather_api_key", None)
-    params = {"q": city_to_find, "appid": openweather_api_key}
+        latinfo = get_lat_lon_from_city(city_to_find)
+        if latinfo is None:
+            await event.respond("City not found")
+            return
+        lat, lon = latinfo
 
-    # get geocoding lat log first
-    geocoding_url = "http://api.openweathermap.org/geo/1.0/direct"
-    geocoding_res = get_response(geocoding_url, params)
-    logger.info(f"geocoding response - {geocoding_res}")
-    # await event.respond("done")
-    # return
-
-    lat, lon = geocoding_res[0]["lat"], geocoding_res[0]["lon"]
-
-    if is_forecast is None:
         logger.info(f"Sending current weather info")
         openweather_url = "https://api.openweathermap.org/data/2.5/weather"
         params = {
@@ -49,20 +38,49 @@ async def get_weather(event):
         weather_res = get_response(openweather_url, params)
         tg_response = format_weather_response(weather_res)
         await event.respond(tg_response)
-    # else:
-    #     logger.info(f"sending forecast")
-    #     openweather_url = "https://api.openweathermap.org/data/2.5/forecast"
-    #     days_list = get_response(openweather_url, params)
-    #     tg_response = "**Sending 5 days forecast**\n"
-    #     try:
-    #         for day in range(len(days_list['list'])):
-    #             print("Day ", day);
-    #             tg_response = f"Day **{day+1}**\n"
-    #             days_list['list'][day]['name'] = days_list['city']['name']
-    #             tg_response += format_weather_response(days_list['list'])
-    #         await event.respond(tg_response)
-    #     except Exception as e:
-    #         logger.exception(f"{e}")
+        return
+    elif len(query) == 3 and query[2] == "forecast":
+        logger.info(f"forecast")
+        await event.respond("Forecast not implemented yet")
+        return
+    elif len(query) == 3 and query[2] == "air":
+        city_to_find = query[1]
+        logger.info(f"city to find - {city_to_find}")
+
+        latinfo = get_lat_lon_from_city(city_to_find)
+        if latinfo is None:
+            await event.respond("City not found")
+            return
+        lat, lon = latinfo
+
+        logger.info(f"Sending current air pollution info")
+        openweather_url = "https://api.openweathermap.org/data/2.5/air_pollution"
+        params = {
+            "lat": lat,
+            "lon": lon,
+            "appid": openweather_api_key,
+            "units": "metric",
+        }
+        weather_res = get_response(openweather_url, params)
+        tg_response = format_air_pollution_response(weather_res, city_to_find)
+        await event.respond(tg_response)
+
+
+def get_lat_lon_from_city(city_to_find):
+    # Prepare request
+    openweather_api_key = environ.get("openweather_api_key", None)
+    params = {"q": city_to_find, "appid": openweather_api_key}
+
+    # get geocoding lat log first
+    geocoding_url = "http://api.openweathermap.org/geo/1.0/direct"
+    geocoding_res = get_response(geocoding_url, params)
+    if len(geocoding_res) == 0:
+        # logger.info(f"geocoding response - {geocoding_res}")
+        return
+    # logger.info(f"geocoding response - {geocoding_res}")
+
+    lat, lon = geocoding_res[0]["lat"], geocoding_res[0]["lon"]
+    return lat, lon
 
 
 def get_response(url, params):
@@ -72,7 +90,7 @@ def get_response(url, params):
 
 def format_weather_response(weather_res):
     try:
-        logger.info(f"request to format {weather_res}")
+        # logger.info(f"request to format {weather_res}")
         temp_in_celcius = weather_res["main"]["temp"]
         feels_like = weather_res["main"]["feels_like"]
         temp_min = weather_res["main"]["temp_min"]
@@ -115,3 +133,57 @@ Clouds:  __{cloud_coverage}%__ Coverage
     except Exception as e:
         print("Error", weather_res)
         logger.exception(f"Error in formatting {e}")
+
+
+def format_air_pollution_response(air_response, location):
+    print(air_response)
+
+    aqi_emojis_dict = {
+        "1": "🟢",
+        "2": "🟡",
+        "3": "🟠",
+        "4": "🔴",
+        "5": "🟤",
+    }
+
+    aqi = air_response["list"][0]["main"]["aqi"] 
+    co2 = air_response["list"][0]["components"]["co"]
+    no = air_response["list"][0]["components"]["no"]
+    no2 = air_response["list"][0]["components"]["no2"]
+    o3 = air_response["list"][0]["components"]["o3"]
+    so2 = air_response["list"][0]["components"]["so2"]
+    pm2_5 = air_response["list"][0]["components"]["pm2_5"]
+    pm10 = air_response["list"][0]["components"]["pm10"]
+    nh3 = air_response["list"][0]["components"]["nh3"]
+
+    return f"""
+Air Pollution: __{location}__
+
+Air Quality Index: {aqi_emojis_dict[str(aqi)]} __{aqi}__
+
+CO: __{co2} μg/m³__ (Carbon Monoxide)
+NO: __{no} μg/m³__ (Nitrogen Monoxide)
+NO2: __{no2} μg/m³__ (Nitrogen Dioxide)
+O3: __{o3} μg/m³__ (Ozone)
+SO2: __{so2} μg/m³__ (Sulfur Dioxide)
+PM2.5: __{pm2_5} μg/m³__ (Fine Particulate Matter)
+PM10: __{pm10} μg/m³__ (Coarse Particulate Matter)
+NH3: __{nh3} μg/m³__ (Ammonia)
+
+"""
+
+
+
+""" 
+note:
+```
+CO - Carbon Monoxide
+NO - Nitrogen Monoxide
+NO2 - Nitrogen Dioxide
+O3 - Ozone
+SO2 - Sulfur Dioxide
+PM2.5 - Fine Particulate Matter
+PM10 - Coarse Particulate Matter
+NH3 - Ammonia
+```
+"""
